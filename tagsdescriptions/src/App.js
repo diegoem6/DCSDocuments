cddb807f8c42453a93d4be44421cf84a3ac1df5c
unknown <diegoem6@gmail.com>
2021-05-31 13:11:11 -0300

    cambios en el app

--- tagsdescriptions/src/App.js
+++ tagsdescriptions/src/App.js
@@ -11,7 +11,7 @@ import AssetState from './context/asset/assetState';
 import SystemState from './context/system/systemState';
 import Menu from './layout/menu';
 import TagDescriptorState from './context/tagdescriptor/tagDescriptorState';
-import Docpdf from './components/tagsdescriptors/docpdf'
+//import Docpdf from './components/tagsdescriptors/docpdf'
 import Eventlist from './components/alarmasyeventos/eventlist'
 
 
@@ -40,7 +40,6 @@ function App() {
                   <Route exact path="/newuser" component={Newuser}/>
                   <PrivateRoute exact path="/tagsdescriptors" component={Tagsdescriptors}/>
                   <PrivateRoute exact path="/assets" component={Assets}/>
-                  <PrivateRoute exact path="/docpdf" component={Docpdf}/>
                   <PrivateRoute exact path="/menu" component={Menu}/>
                   <PrivateRoute exact path="/events" component={Eventlist}/>
                 </Switch>
