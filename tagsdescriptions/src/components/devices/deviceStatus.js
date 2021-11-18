e63840cf3400c54ea3b4d505861d523b36ed52da
unknown <diegoem6@gmail.com>
2021-11-18 16:22:00 -0300

    Update deviceStatus.js

--- tagsdescriptions/src/components/devices/deviceStatus.js
+++ tagsdescriptions/src/components/devices/deviceStatus.js
@@ -37,7 +37,7 @@ const DeviceStatus = () => {
         if(devicetype){
             setDeviceTipo(devicetype.type)
             deviceSelected.type_desc =  devicetype
-            
+            deviceSelected.status = deviceStatusC300v2
         }
     }, [devicetype])
 
