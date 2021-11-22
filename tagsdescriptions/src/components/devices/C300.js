6be18ddf20a97c971f2f5806ea186423e037d260
pabloiadema <62518128+pabloiadema@users.noreply.github.com>
2021-11-22 18:10:22 -0300

    nuevo C300

--- tagsdescriptions/src/components/devices/C300.js
+++ tagsdescriptions/src/components/devices/C300.js
@@ -1,8 +1,20 @@
 import React, { Fragment } from 'react';
 
 const C300 = ({deviceSelected}) => {
-    console.log(deviceSelected)
-    console.log(deviceSelected.status_)
+    //console.log(deviceSelected)
+    //console.log(deviceSelected.status_)
+    
+    const renderSwitch = (param) => {
+        console.log(param)
+        switch(param) {
+            case 0: return <img src = "/img/icon_green.svg.png" className="img_status_interface"/>
+            case 1: return <img src = "/img/icon_yellow.svg.png" className="img_status_interface"/>
+            case 2: return <img src = "/img/icon_red.svg.png" className="img_status_interface"/>
+            case 3: return <img src = "/img/icon_grey.svg.png" className="img_status_interface"/>
+            default: return <img src = "/img/icon_grey.svg.png" className="img_status_interface"/>
+        }
+    }
+
     return (
         <div className="device_grid">
                 {deviceSelected.type_desc?
@@ -11,68 +23,36 @@ const C300 = ({deviceSelected}) => {
                 }
                 {deviceSelected.status_.c300 ? 
                     (
-                    <div>
-                        <div className="device_div">
+                    <Fragment>
+                        <div className="device_div" >
                             <table className="device_table">
                                 <tbody className="device_table">
                                     <tr>
                                         <th width="50%">Properties</th>
                                         <th width="50%"></th>
                                     </tr>
-                                    <tr>
-                                        <td width="50%"><b>{deviceSelected.status_.c300.state[0].label}</b></td>
-                                        <td width="50%" key="nombre">{deviceSelected.status_.c300.state[0].value}</td>
-                                    </tr>
-                        
-                                    <tr>
-                                        <td width="50%"><b>{deviceSelected.status_.c300.state[1].label} &nbsp; &nbsp; &nbsp;
-                                        {deviceSelected.status_.c300.state[1].value === 0 ? 
-                                            (<img src = "/img/icon_green.svg.png" className="img_status_interface"/>)
+                                    {deviceSelected.status_.c300.state.map(datos=>{ //aca se usa map y despues un return, en javascript: foreach
+                                        return(<tr>
+                                            <td width="50%"><b>{datos.label}</b></td>
+                                            {datos.type === "icon" ?
+                                                (datos.value === 0 ? 
+                                                    <img src = "/img/icon_green.svg.png" className="img_status_interface"/>
+                                                    : (<img src = "/img/icon_red.svg.png" className="img_status_interface"/>))
                                             :
-                                            (<img src = "/img/icon_red.svg.png" className="img_status_interface"/>)
-                                        }
-                                        </b></td>
-                                        <td width="50%"><b>{deviceSelected.status_.c300.state[2].label} &nbsp; &nbsp; &nbsp;
-                                        {deviceSelected.status_.c300.state[2].value === 0 ? 
-                                            (<img src = "/img/icon_green.svg.png" className="img_status_interface"/>)
+                                                null
+                                            }
+                                            {(datos.type === "icon4") ?
+                                                renderSwitch(datos.value)
                                             :
-                                            (<img src = "/img/icon_red.svg.png" className="img_status_interface"/>)
-                                        }
-                                        </b></td>
-                                        
-                                    </tr>
-                                    <tr>
-                                        <td width="50%"><b>{deviceSelected.status_.c300.state[3].label}</b></td>
-                                        <td width="50%" key="nombre">{deviceSelected.status_.c300.state[3].value}</td>
-                                    </tr>
-                                    <tr>
-                                        <td width="50%"><b>{deviceSelected.status_.c300.state[4].label}</b></td>
-                                        <td width="50%" key="nombre">{deviceSelected.status_.c300.state[4].value}</td>
-                                    </tr>
-                                    <tr>
-                                        <td width="50%"><b>{deviceSelected.status_.c300.state[5].label}</b></td>
-                                        <td width="50%" key="nombre">{deviceSelected.status_.c300.state[5].value}</td>
-                                    </tr>
-                                    <tr>
-                                        <td width="50%"><b>{deviceSelected.status_.c300.state[6].label}</b></td>
-                                        <td width="50%" key="nombre">
-                                        {deviceSelected.status_.c300.state[6].value === 0 ? 
-                                            (<img src = "/img/icon_green.svg.png" className="img_status_interface"/>)
+                                                null
+                                            }
+                                            {(datos.type === "value") ?
+                                                <td width="50%" key="nombre"><b>{datos.value}</b></td>
                                             :
-                                            (<img src = "/img/icon_red.svg.png" className="img_status_interface"/>)
-                                        }    
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td width="50%"><b>{deviceSelected.status_.c300.state[7].label}</b></td>
-                                        <td width="50%" key="nombre">
-                                        {deviceSelected.status_.c300.state[7].value === 0 ? 
-                                            (<img src = "/img/icon_green.svg.png" className="img_status_interface"/>)
-                                            :
-                                            (<img src = "/img/icon_red.svg.png" className="img_status_interface"/>)
-                                        }     
-                                        </td>
-                                    </tr>
+                                                null
+                                            }
+                                        </tr>)
+                                    })}
                                 </tbody>
                             </table>
                         </div>   
@@ -87,12 +67,23 @@ const C300 = ({deviceSelected}) => {
                                         deviceSelected.status_.c300.softfailure.map( failure => {
                                             return (<tr>
                                                 <td width="50%"><b>{failure.label}</b></td>
-                                                <td width="50%">{failure.value === 0 ? 
-                                                    (<img src = "/img/icon_green.svg.png" className="img_status_interface"/>)
-                                                    :
-                                                    (<img src = "/img/icon_red.svg.png" className="img_status_interface"/>)
+                                                {failure.type === "icon" ?
+                                                    (failure.value === 0 ? 
+                                                        <img src = "/img/icon_green.svg.png" className="img_status_interface"/>
+                                                        : (<img src = "/img/icon_red.svg.png" className="img_status_interface"/>))
+                                                :
+                                                    null
                                                 }
-                                                </td>
+                                                {(failure.type === "icon4") ?
+                                                    renderSwitch(failure.value)
+                                                :
+                                                    null
+                                                }
+                                                {(failure.type === "value") ?
+                                                    <td width="50%" key="nombre"><b>{failure.value}</b></td>
+                                                :
+                                                    null
+                                                }
                                             </tr>)
                                         })
                                     }
@@ -100,7 +91,7 @@ const C300 = ({deviceSelected}) => {
                                 </tbody>        
                             </table>
                         </div>
-                    </div>
+                    </Fragment>
                 )       
                 :console.log(deviceSelected.status_.c300)}
             </div>
